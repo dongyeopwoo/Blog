c7483446f1fa0e80ed51828c5c65c4028ae3ab36
dongyeopwoo <dongyeopwoo@naver.com>
2024-08-03 23:59:58 +0900

    2024-08-03-11:59

---
+++ js/index.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "웹과 앱을 개발 하고 있는 우동엽 입니다 :)";
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      document.getElementById("typing").innerHTML += text.charAt(index);
+      index++;
+      setTimeout(type, 100);
+    }
+  }
+
+  type();
+
+  const elements = document.querySelectorAll(
+    ".continer1, .continer2, .continer3, .continer4"
+  );
+
+  function checkScroll() {
+    elements.forEach((element) => {
+      const rect = element.getBoundingClientRect();
+      if (rect.top <= window.innerHeight - 100) {
+        element.classList.add("fadeInUp");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkScroll);
+  checkScroll();
+});
